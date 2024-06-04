4c30759d0912a2b9ed5107be945cefd30803bf3b
28sungmin <gooddays4001@naver.com>
2024-06-04 21:05:15 +0900

    modify favicon

--- src/components/common/HeaderCp.jsx
+++ src/components/common/HeaderCp.jsx
@@ -1,5 +1,36 @@
+import logoImg from "../../assets/images/title.png";
+
 const HeaderCp = () => {
-  return <></>;
+  return (
+    <header className="header-space h-60">
+      <div className="wrap d-flex justify-content-between alignt-items-center h-100-per">
+        <div className="logo">
+          <a href="/">
+            <img src={logoImg} alt="거북이 로고" />
+          </a>
+        </div>
+        <nav>
+          <div className="d-flex">
+            <li>
+              <a href="/">도서 설명</a>
+            </li>
+            <li>
+              <a href="/">저자 소개</a>
+            </li>
+            <li>
+              <a href="/">목차</a>
+            </li>
+            <li>
+              <a href="/">터틀 함수</a>
+            </li>
+            <li>
+              <a href="/">터틀 작품</a>
+            </li>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default HeaderCp;
